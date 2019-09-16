add94b1472a9bf796c204ff3b23fb7bde80003df
Esteban <estebanchavarriacr@gmail.com>
2019-09-15 20:32:05 -0600

    Quickity fixy

--- futbolFork.c
+++ futbolFork.c
@@ -8,18 +8,28 @@
 #include <unistd.h>
 #include <signal.h>
 #include <pthread.h>
-#include <sys/mman.h>
 
 
 #define PLAYERS_PER_TEAM 5
 
-//Prototipos de funciones
-void* create_shared_memory(size_t);
-
 //Globals cos reasons
-pthread_mutex_t lock; //Borrar creo
+pthread_mutex_t lock;
 volatile sig_atomic_t usr_interrupt = 0;
+/*
 
+struct semaphore{
+    int* avaible;
+    int* resource;
+};
+
+struct semaphore* new_semaphore(int* pResource) {
+    struct semaphore* sem = malloc(sizeof(struct semaphore));
+    sem->avaible = (int*) malloc(2);
+    *(sem-> avaible) = 1;
+    sem -> resource = pResource;
+    return sem;
+}
+*/
 
 void synch_signal (int sig){
   usr_interrupt = 1;
@@ -40,6 +50,7 @@ void* create_shared_memory(size_t size) {
 struct Nodo{
   pid_t* data;
   struct Nodo *siguiente;
+
 };
 
 struct Nodo* new_nodo(pid_t data) {
@@ -88,6 +99,7 @@ void push(struct Cola *cola, pid_t data){
   }
 }
 
+
 struct Semaphore{
   int *value;
   int *resource;
@@ -113,18 +125,9 @@ void wait_semaphore(struct Semaphore* sem, sigset_t* set){
     //Se debe agregar a la lista de procesos que esperan el recurso
     push(sem->cola, getpid());
     //Se suspende el proceso para que espere por el recurso
-    sigset_t mask, oldmask;
-    /* Se establece la mask de las senales que se van a bloquear temporalmente */
-    sigemptyset (&mask);
-    sigaddset (&mask, SIGUSR1);
-    /* Se espera por la senal SIGUSR1 */
-    sigprocmask (SIG_BLOCK, &mask, &oldmask);
-    //while (!usr_interrupt)
-      //sigsuspend (&oldmask);
-    sigsuspend (&oldmask);
-    sigprocmask (SIG_UNBLOCK, &mask, NULL);
-
-  }void* create_shared_memory(size_t size) 
+    sigwait(set, &return_val);
+    printf("Soy el siguiente y recibi signal, soy: %d\n", getpid());
+  }
 }
 
 //El proceso que ya uso el recurso lo notifica
@@ -134,10 +137,25 @@ void signal_semaphore (struct Semaphore* sem){
   printf("VALOR DESPUES SIGNAL > %d\n", *(sem -> value));
   if(*(sem -> value) <= 0){//Hay procesos esperando
     pid_t process_wakeup = pop(sem->cola);
+    printf("El proceso a despertar es: %d\n", process_wakeup);
     kill(process_wakeup, SIGUSR1);//Suena que lo mato, pero no es asi
   }
   printf("Solte el recurso, soy: %d\n", getpid());
 }
+
+/*
+int adquire(struct semaphore* sem){
+    if (*(sem->avaible) == 0) return 0;
+
+    *(sem->avaible) = 0;
+    return 1;
+}
+
+void release(struct semaphore* sem){
+    *(sem->avaible) = 1;
+    return;
+}
+*/
 
 //Permite hacer operaciones de forma atomica
 int compare_and_swap(int *value, int expected, int new_value){
@@ -147,16 +165,8 @@ int compare_and_swap(int *value, int expected, int new_value){
   return temp;
 }
 
-void* create_shared_memory(size_t size) {
-  // Readable and writable:
-  int protection = PROT_READ | PROT_WRITE;
 
-  //Visibility defined so
-  // Only this process and its children will be able to use it:
-  int visibility = MAP_SHARED | MAP_ANONYMOUS;
 
-  return mmap(NULL, size, protection, visibility, -1, 0);
-}
 
 int main(){
     //Signals info
@@ -165,29 +175,34 @@ int main(){
     sigprocmask(SIG_BLOCK, &set, NULL);
 
     //Program
-    int* goalA = (int*) create_shared_memory( sizeof(int) );
-    int* goalB = (int*) create_shared_memory( sizeof(int) );
-    int* ball  = (int*) create_shared_memory( sizeof(int) );
+    int* goalA = create_shared_memory( sizeof(int) );
+    int* goalB = create_shared_memory( sizeof(int) );
+
+    int* ball = create_shared_memory( sizeof(int) );
+
+    int tiempoEspera;
 
     bool* inicioPartido = create_shared_memory( sizeof(bool) );
-    
     *inicioPartido = false;
     bool* finPartido = create_shared_memory( sizeof(bool) );
     *finPartido = false;
 
     struct Semaphore *semaphoreBall = new_semaphore(ball);
 
-    int tiempoEspera;
-    
+    //int* p = (int*) malloc(2); //Pruebas
+    //*p = 0;
+
+    //struct semaphore* semP = new_semaphore(p);
+
     int cantidadJugadores = 10;
     pid_t parentID = getpid();
     pid_t arrayPIDs[cantidadJugadores];//Se almacenan los procesos hijos
     int c = 0;//Contador
     char equipo;
     printf("Arbiter ID: %d.!\n", parentID);
-    
     //Se crean los hijos
     do {
+
   		pid_t pid;
 
   		pid = fork();
@@ -195,12 +210,16 @@ int main(){
   		if( pid < 0 ) {
   			fprintf(stderr, "Fork Fail");
   			return 1;
-  		} else if ( pid == 0 ) {
+  		}
+  		else if ( pid == 0 ) {
   			//execlp("/bin/ls", "ls", NULL);
-            equipo = (c >= cantidadJugadores/2) ? 'B' : 'A';
-  		    printf("Equipo %c | Proceso Hijo creado: #%d, proceso padre #%d \n", equipo, getpid() , getppid()); //El hijo está saludando
-            break;
-  		} else { //Parent process
+        equipo = (c >= cantidadJugadores/2) ? 'B' : 'A';
+  			printf("Equipo %c | Proceso Hijo creado: #%d, proceso padre #%d \n", equipo, getpid() , getppid()); //El hijo está saludando
+
+        break;
+  		}
+  		else { //Parent process
+  			//printf("The parent is creating more childs\n");//El padre va a seguir creando hijs
   			arrayPIDs[c] = pid;//Guardo el ID del hijo creado para poder esperarlo más tarde
   		}
 
@@ -239,6 +258,43 @@ int main(){
       }
 
   	}
+
+    /*
+    for (int generated = 0; generated < PLAYERS_PER_TEAM; generated++){
+        if (getpid() == parentID) pid = fork();
+    }
+
+    //Hijos por otro camino
+
+    for (int generated = 0; generated < PLAYERS_PER_TEAM; generated++){
+        if (getpid() == parentID) pid = fork();
+    }
+
+    //
+
+    if (pid < 0){
+        printf("Welp I failed\n");
+        return 1;
+    } else if (pid > 0){
+        printf("[Arbiter]: Start! \n");
+        //Waits for 5min before killing everyone
+        wait(NULL);
+        printf("ball %d\n", *p); //No one is using it so :p
+    } else {
+
+        if (adquire(semP) == 0){
+            printf("[Player]: Almost\n");
+        } else {
+            printf("[Player]: It's mine\n");
+            sleep(2);
+            printf("[Player]: There, have it\n");
+            release(semP);
+        }
+
+        //printf("[Player]: I'm a player process\n");
+        sleep(2);
+    }
+    */
     return 0;
 
 }
